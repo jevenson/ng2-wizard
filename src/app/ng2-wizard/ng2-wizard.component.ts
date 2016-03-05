5d65541980be1d1e112085a7415b1660b0594ac8
Josh Evenson <jevenson@outlook.com>
2016-03-05 15:55:19 -0600

    updated to ngbeta8. Next and previous buttons for tabwizard

--- src/app/ng2-wizard/ng2-wizard.component.ts
+++ src/app/ng2-wizard/ng2-wizard.component.ts
@@ -7,18 +7,74 @@ import { Ng2WizardTab } from './ng2-wizard-tab.component';
 })
 export class Ng2Wizard implements AfterContentInit {
     
+    // Allow usage of enum in template
+    private NavigationDirection = NavigationDirection;
+    
     @ContentChildren(Ng2WizardTab)
-    tabs: QueryList<Ng2WizardTab>;
+    private tabs: QueryList<Ng2WizardTab>;
+    
+    private get activeTab(): Ng2WizardTab {
+        return this.tabs.toArray().find(tab => tab.active);
+    }
+    
+    private get currentStepIndex(): number {
+        return this.tabs.toArray().indexOf(this.activeTab);
+    }
+    
+    private get hasNextStep(): boolean {
+        return this.currentStepIndex < this.tabs.length - 1;
+    }
+    
+    private get hasPreviousStep(): boolean {
+        return this.currentStepIndex > 0;
+    }
     
     public ngAfterContentInit(): void {
         this.tabs.first.active = true;
     }
     
     private onTabClick(selectedTab: Ng2WizardTab): void {
-        this.tabs.toArray().forEach((tab) => {
-            tab.active = false;
-        });
-        
+        this.deactivateAllTabs();
         selectedTab.active = true;
     }
+    
+    private onButtonClick(direction: NavigationDirection): void {
+        switch (direction) {
+            case NavigationDirection.NEXT:
+                this.next();
+                break;
+            case NavigationDirection.PREVIOUS:
+                this.previous();
+                break;
+            default:
+                throw new Error(direction + " is not a valid NavigationDirection");
+        }
+    }
+    
+    private next(): void {
+        if (this.hasNextStep) {
+            let tab: Ng2WizardTab = this.tabs.toArray()[this.currentStepIndex + 1];
+            this.deactivateAllTabs();
+            tab.active = true;
+        }
+    }
+    
+    private previous(): void {
+        if (this.hasPreviousStep) {
+            let tab: Ng2WizardTab = this.tabs.toArray()[this.currentStepIndex - 1];
+            this.deactivateAllTabs();
+            tab.active = true;
+        }
+    }
+    
+    private deactivateAllTabs(): void {
+        this.tabs.forEach((tab) => {
+            tab.active = false;
+        });
+    }
+}
+
+enum NavigationDirection {
+    PREVIOUS,
+    NEXT
 }
